1f63c42818608e6c7baa0445f46b46ec2d0fd148
unknown <francoisvdven@gmail.com>
2013-01-14 12:09:50 +0100

    Fixed base perf issue

--- Unity/Assets/Scripts/Base.cs
+++ Unity/Assets/Scripts/Base.cs
@@ -7,6 +7,15 @@ public class Base : MonoBehaviour, INetworkListener
     void Start()
     {
         NetworkManager.Instance.Client.AddListener(this);
+        
+        GameObject t = GameObject.Find("Terrain");
+        TerrainCollider tc = t.GetComponentInChildren<TerrainCollider>();
+
+        Component[] colliders = GetComponentsInChildren(typeof(MeshCollider));
+        foreach (Collider c in colliders)
+        {
+            Physics.IgnoreCollision(tc, c);
+        }
     }
 
     public void OnDataReceived(DataPackage dp)
